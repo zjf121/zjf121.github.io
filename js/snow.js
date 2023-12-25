e177a34c91052986ee0278f37d067c86f8919203
zjf121 <3352629190@qq.com>
2023-12-25 21:46:38 +0800

    new

--- js/snow.js
+++ js/snow.js
@@ -1,41 +1,104 @@
-(function($){
-   $.fn.snow = function(options){
-   var $flake = $('<div id="snowbox" />').css({'position': 'absolute','z-index':'9999', 'top': '-50px'}).html('❄'),
-   documentHeight     = $(document).height(),
-   documentWidth  = $(document).width(),
-   defaults = {
-      minSize   : 10,
-      maxSize   : 20,
-      newOn     : 1000,
-      flakeColor : "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
-   },
-   options = $.extend({}, defaults, options);
-   var interval= setInterval( function(){
-   var startPositionLeft = Math.random() * documentWidth - 100,
-   startOpacity = 0.5 + Math.random(),
-   sizeFlake = options.minSize + Math.random() * options.maxSize,
-   endPositionTop = documentHeight - 200,
-   endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
-   durationFall = documentHeight * 10 + Math.random() * 5000;
-   $flake.clone().appendTo('body').css({
-      left: startPositionLeft,
-      opacity: startOpacity,
-      'font-size': sizeFlake,
-      color: options.flakeColor
-   }).animate({
-      top: endPositionTop,
-      left: endPositionLeft,
-      opacity: 0.2
-   },durationFall,'linear',function(){
-        $(this).remove()
-   });
-   }, options.newOn);
-    };
-    })(jQuery);
-$(function(){
-    $.fn.snow({
-       minSize: 5, /* 定义雪花最小尺寸 */
-       maxSize: 50,/* 定义雪花最大尺寸 */
-       newOn: 300  /* 定义密集程度，数字越小越密集 */
-    });
-});
+var snow = {
+	/**
+	 * float  The current x position
+	 */
+	x: 0,
+
+	/**
+	 * float  The current y position
+	 */
+	y: 0,
+
+	/**
+	 * float  Alpha
+	 */
+	alpha: 0.5,
+
+	/**
+	 * float  The radius of the circle.
+	 */
+	radius: 3,
+
+	/**
+	 * Velocity for the particle.
+	 */
+	velocity: {
+		x: 0,
+		y: 5
+	},
+
+	/**
+	 * Draws the particle onto the canvas in context.
+	 *
+	 * @param	c	 The canvas context to draw onto
+	 */
+	draw:function(c){
+		c.fillStyle = 'rgba(255,255,255,'+this.alpha+')';
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+		c.fill();
+	},
+
+	/**
+	 * Updates the particle.
+	 */
+	update:function(){
+		this.x += this.velocity.x
+		this.y += this.velocity.y;
+
+
+	},
+
+	/**
+	 * Randomizes the settings for the particle
+	 *
+	 * @param	zone	The blastZone
+	 */
+	randomize:function(zone){
+		// Get a random point inside the blastzone and set the x/y
+		var s = this.getLocation(zone);
+		this.x = s.x;
+		this.y = s.y;
+
+		this.alpha = this.randomRange(0.3, 1);
+		this.radius = this.randomRange(1, 5);
+
+		this.velocity = {
+			x: this.randomRange(-0.35, 0.35),
+			y: this.randomRange(0.75, 1.5)
+		}
+	},
+
+	/**
+	 * Adds a random drift type motion to the particle.
+	 * Taken from the Flint particle system....
+	 */
+	action:function(){
+		this.velocity.x += (Math.random() - 0.5) * 0.1;
+	},
+
+	/**
+	 * Take a low and high number and gets a random number between them.
+	 *
+	 * @param	low		The low number
+	 * @param	high	The high number
+	 * @return	Number
+	 */
+	randomRange:function(low, high){
+		return (Math.random() * (high - low)) + low;
+	},
+
+	/**
+	 * Gets a random starting point for the particle.
+	 * Taken from the Flint particle system in AS3
+	 *
+	 * @param	zone	The zone to get a location from
+	 * @return	Object
+	 */
+	getLocation:function(z){
+		var p = {};
+		p.x = z.x + Math.random() * z.width;
+		p.y = z.y + Math.random() * z.height;
+		return p;
+	}
+};
