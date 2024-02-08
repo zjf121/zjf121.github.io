b1ca6ddba15484a5ad0a27fc9f461b234de3ea94
zjf121 <3352629190@qq.com>
2024-02-08 21:16:20 +0800

    修改背景图片

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["新年快乐", "心想事成"];
+const randomWords = ["樊雨静", "小小静","祝樊雨静新年快乐","新的一年樊雨静肠胃棒棒"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
